0530c487a60da90305e36d84c2f65780c53e7473
Uti-Favour <utifavour15@gmail.com>
2023-08-06 08:13:24 +0100

    Landing Page Concept

--- Home Page/script.js
+++ Home Page/script.js
@@ -1,10 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
- let btn = next;
- next = 20 * 30;
-
- console.log(btn);
-
- let a = [
-    firstName = favour,
- ];
+   const landingPageVideo = document.getElementById('landingPageVideo');
+   const playButton = document.getElementById('playButton');
+   const removeButton = document.getElementById('removeButton');
+   
+   playButton.addEventListener('click', () => {
+     landingPageVideo.play();
+     playButton.style.display = 'none';
+     removeButton.style.display = 'block';
+   });
+   
+   removeButton.addEventListener('click', () => {
+     landingPageVideo.pause();
+     landingPageVideo.currentTime = 0;
+     playButton.style.display = 'block';
+     removeButton.style.display = 'none';
+   });
+   
+   landingPageVideo.addEventListener('click', () => {
+     if (landingPageVideo.requestFullscreen) {
+       landingPageVideo.requestFullscreen();
+     } else if (landingPageVideo.mozRequestFullScreen) {
+       landingPageVideo.mozRequestFullScreen();
+     } else if (landingPageVideo.webkitRequestFullscreen) {
+       landingPageVideo.webkitRequestFullscreen();
+     } else if (landingPageVideo.msRequestFullscreen) {
+       landingPageVideo.msRequestFullscreen();
+     }
+   });
+   
+   document.addEventListener('fullscreenchange', () => {
+     if (!document.fullscreenElement) {
+       landingPageVideo.pause();
+       landingPageVideo.currentTime = 0;
+       playButton.style.display = 'block';
+       removeButton.style.display = 'none';
+     }
+   });
+   
+   
 });
